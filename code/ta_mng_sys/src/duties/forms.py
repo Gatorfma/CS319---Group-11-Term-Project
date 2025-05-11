3fba1722a06be7c40cca415147457b3381c81866
komacyus <furkankomac@gmail.com>
2025-05-11 12:26:54 +0300

    little changes on log duty logic

--- code/ta_mng_sys/src/duties/forms.py
+++ code/ta_mng_sys/src/duties/forms.py
@@ -2,67 +2,60 @@ from django import forms
 
 from courses.models import Exam
 
-# code/ta_mng_sys/src/duties/forms.py
 from django import forms
 from django.utils.timezone import now
 from courses.models import CourseOffering
 from duties.models import LabDuty, GradingDuty, RecitationDuty, OfficeHourDuty, ProctoringDuty
 from django.db.models import Q
 
-DUTY_TYPE_CHOICES = [
-    ('lab',       'Lab Duty'),
-    ('grading',   'Grading Duty'),
-    ('recitation','Recitation Duty'),
-    ('officehour','Office Hour Duty'),
-    ('proctoring','Proctoring Duty'),
-]
+DUTY_MODEL_MAP = {
+    'lab':        LabDuty,
+    'grading':    GradingDuty,
+    'recitation': RecitationDuty,
+    'officehour': OfficeHourDuty,
+    'proctoring': ProctoringDuty,
+}
 
 class DutyLogForm(forms.Form):
+    duty_type = forms.ChoiceField(
+        label="Duty Type",
+        choices=[(k, k.capitalize()) for k in DUTY_MODEL_MAP.keys()],
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
     offering = forms.ModelChoiceField(
         label="Course & Section",
         queryset=CourseOffering.objects.none(),
         widget=forms.Select(attrs={'class': 'form-select'})
     )
-    duty_type = forms.ChoiceField(
-        label="Duty Type",
-        choices=[
-            ('lab',       'Lab Duty'),
-            ('grading',   'Grading Duty'),
-            ('recitation','Recitation Duty'),
-            ('officehour','Office Hour Duty'),
-            ('proctoring','Proctoring Duty'),
-        ],
-        widget=forms.Select(attrs={'class': 'form-select'})
-    )
     minutes_worked = forms.IntegerField(
         label="Minutes Worked",
         min_value=1,
-        widget=forms.NumberInput(attrs={
-            'class': 'form-input',
-            'placeholder': 'Enter minutes worked'
-        })
+        widget=forms.NumberInput(attrs={'class': 'form-input'})
     )
 
     def __init__(self, *args, user=None, **kwargs):
         super().__init__(*args, **kwargs)
         if not user or not hasattr(user, 'ta_profile'):
             return
-
         ta = user.ta_profile
 
-        # Build a queryset of EVERY CourseOffering where the TA is assigned on any duty type
+        selected = self.data.get('duty_type')
+        if selected in DUTY_MODEL_MAP:
+            model_cls     = DUTY_MODEL_MAP[selected]
+            duty_qs       = model_cls.objects.filter(assigned_ta=ta)
+            offering_ids = duty_qs.values_list('offering_id', flat=True)
+            qs = CourseOffering.objects.filter(pk__in=offering_ids)
+        else:
+            qs = CourseOffering.objects.filter(
+                Q(labdutyduties__assigned_ta=ta) |
+                Q(gradingdutyduties__assigned_ta=ta) |
+                Q(recitationdutyduties__assigned_ta=ta) |
+                Q(officehourdutyduties__assigned_ta=ta) |
+                Q(proctoringdutyduties__assigned_ta=ta)
+            ).distinct()
 
-        qs = CourseOffering.objects.filter(
-            Q(labdutyduties__assigned_ta=ta) |
-            Q(gradingdutyduties__assigned_ta=ta) |
-            Q(recitationdutyduties__assigned_ta=ta) |
-            Q(officehourdutyduties__assigned_ta=ta) |
-            Q(proctoringdutyduties__assigned_ta=ta)
-        ).distinct().order_by('course__course_code')
+        self.fields['offering'].queryset = qs.order_by('course__course_code')
 
-        self.fields['offering'].queryset = qs
-
-        # Make each <option> read “CS101 – Intro to Programming (Section 1)”
         self.fields['offering'].label_from_instance = lambda o: (
             f"{o.course.course_code} – {o.course.title} (Section {o.section})"
         )
