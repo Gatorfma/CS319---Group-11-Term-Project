93010b17c1d1458ac138def2876f44103388a696
Alper-Bicer <alper.bicer@ug.bilkent.edu.tr>
2025-05-11 12:40:41 +0300

    Merged Furkan-A into Alper-B

--- code/ta_mng_sys/src/courses/urls.py
+++ code/ta_mng_sys/src/courses/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import import_data_view
+from . import views
 
 app_name = "courses"
 
 urlpatterns = [
     path('import-data/', import_data_view, name='import_data'),
+    path("upload_student_excell/",  views.upload_student_excell, name="upload_student_excell"),
 ]
